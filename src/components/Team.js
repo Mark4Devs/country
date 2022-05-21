ef5f5f7b4997fcac7dc45d315536192c1f76a5c5
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-21 15:56:48 +0300

    Create Team.js

---
+++ src/components/Team.js
@@ -0,0 +1,70 @@
+import {Carousel } from 'react-bootstrap';
+function Team(){
+    return(
+        <div className='team-container'>
+            <div className='border-container'>
+                <div className='hr'></div>
+                <h2>НАША КОМАНДА</h2>
+                <div className='hr'></div>
+            </div>
+            <div className="slider">
+                <Carousel interval={null}>
+                    <Carousel.Item>
+                    <div className='image-holder'>
+                            <img src="images/team-member-1.jpg"></img>
+                            <img src="images/team-member-2.jpg"></img>
+                            <img src="images/team-member-3.jpg"></img>
+                            <img src="images/team-member-4.jpg"></img>
+                            <img src="images/team-member-5.jpg"></img>
+                    </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='image-holder'>
+                            <img src="images/team-member-6.jpg"></img>
+                            <img src="images/team-member-7.jpg"></img>
+                            <img src="images/team-member-1.jpg"></img>
+                            <img src="images/team-member-2.jpg"></img>
+                            <img src="images/team-member-3.jpg"></img>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='image-holder'>
+                            <img src="images/team-member-4.jpg"></img>
+                            <img src="images/team-member-5.jpg"></img>
+                            <img src="images/team-member-6.jpg"></img>
+                            <img src="images/team-member-7.jpg"></img>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+                <div className="slider mini">
+                    <Carousel interval= {null}>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-1.jpg"></img>
+                                <img src="images/team-member-2.jpg"></img>
+                                <img src="images/team-member-3.jpg"></img>
+                                <img src="images/team-member-4.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <div className='image-holder'>
+                            <img src="images/team-member-5.jpg"></img>
+                            <img src="images/team-member-6.jpg"></img>
+                            <img src="images/team-member-7.jpg"></img>
+                        </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-7.jpg"></img>
+                                <img src="images/team-member-5.jpg"></img>
+                                <img src="images/team-member-6.jpg"></img>
+                                <img src="images/team-member-7.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+        </div>
+)
+    }
+export default Team;
