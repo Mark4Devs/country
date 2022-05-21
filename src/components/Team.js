3da8ad2eaf19913355b2e77063401cdc377a7ae1
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-21 18:58:08 +0300

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -102,6 +102,52 @@ function Team(){
                         </Carousel.Item>
                     </Carousel>
                 </div>
+                <div className="slider-mini-three">
+                    <Carousel interval= {null}>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-1.jpg"></img>
+                                <img src="images/team-member-2.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <div className='image-holder'>
+                            <img src="images/team-member-3.jpg"></img>
+                            <img src="images/team-member-4.jpg"></img>
+                        </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-5.jpg"></img>
+                                <img src="images/team-member-6.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-7.jpg"></img>
+                                <img src="images/team-member-1.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-2.jpg"></img>
+                                <img src="images/team-member-3.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-4.jpg"></img>
+                                <img src="images/team-member-5.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className='image-holder'>
+                                <img src="images/team-member-6.jpg"></img>
+                                <img src="images/team-member-7.jpg"></img>
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
         </div>
 )
     }
