0d037d3a7fa11d362f2ef718b2945b03cab47d76
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 03:10:55 +0300

    Update Donate.js

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -2,6 +2,30 @@
 
 function Donate(){
     return(
+        <div>
+        <header className="header">
+          <Navbar className='bg-light' expand="lg" fixed='top'>
+          <div className='header-outline'>
+              <Button>English</Button>
+              <Button className='btn active'>Українська</Button>
+
+            <div className='header-info'>
+              <span>+380 60 983 08 42</span>
+              <span>countryofpeace@gmail.com</span>
+            </div>
+          </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand to="/"><img src='images/logo.png'/></Navbar.Brand>
+            <Navbar.Collapse className='nav-bar'>
+              <Nav className='navigation'>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Головна</NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Про нас</NavLink>
+                <NavLink to="/donate" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Реквізити</NavLink>
+                <NavLink to="/contacts" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Контакти</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+      </header>
         <div className="container-donate">
             <div className="section">
                 <ul>
@@ -37,6 +61,7 @@ function Donate(){
             </div>      
             
         </div>  
+        </div>
     )
 }
 
