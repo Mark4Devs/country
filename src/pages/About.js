ab8fcaaf70acc7c50942ed2ce1f176119956fca3
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 17:43:17 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -131,7 +131,7 @@ function About(){
                     <span><a href="https://www.facebook.com/Країна-Миру-Country-of-Peace-112502804794164/"><BsFacebook/> Facebook</a></span>
                     <span><a href="https://instagram.com/countryofpeace.ua?igshid=YmMyMTA2M2Y/"><BsInstagram/> Instagram</a></span>  
                     <h5 className='mt-4'>{item['ukr-footer-phone']}</h5>
-                    <span>+380 60 983 08 42</span><br/>
+                    <span>+380 50 983 08 42</span><br/>
                   </div>
                 </div>)) : enItem.map(item => ( <div className='contacts-container'>
                   <h3 className='heading mb-5'>{item['en-footer-heading']}</h3>
