0459c1e7dcb0c00d44327c46bedf3d38f86240db
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 03:11:40 +0300

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,6 +3,30 @@ import { MdLocationOn } from 'react-icons/md';
 import { HiMail } from 'react-icons/hi'; 
 function Contacts(){
     return(
+        <div>
+        <header className="header">
+          <Navbar className='bg-light' expand="lg" fixed='top'>
+          <div className='header-outline'>
+              <Button>English</Button>
+              <Button className='btn active'>Українська</Button>
+
+            <div className='header-info'>
+              <span>+380 60 983 08 42</span>
+              <span>countryofpeace@gmail.com</span>
+            </div>
+          </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Brand to="/"><img src='images/logo.png'/></Navbar.Brand>
+            <Navbar.Collapse className='nav-bar'>
+              <Nav className='navigation'>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Головна</NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Про нас</NavLink>
+                <NavLink to="/donate" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Реквізити</NavLink>
+                <NavLink to="/contacts" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Контакти</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+      </header>
         <div className="contacts-page">
             <div className="contacts-item">
                 <FaPhone/>
@@ -16,6 +40,7 @@ function Contacts(){
                 <HiMail/>
                 <span>countryofpeace@gmail.com</span>
             </div>
+        </div>
         </div>
     )
 }
