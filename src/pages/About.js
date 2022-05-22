fe46eb7e6b2d2ae0a8179dc63a0c39cb30ac6f30
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 11:09:25 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ import Team from '../components/Team';
 import ukrLang from '../db.json';
 import enLang from '../db.json';
 import { useState } from 'react';
+import { BsInstagram, BsFacebook } from 'react-icons/bs';
 
 function About(){    
     const ukrItem = ukrLang.ukrLang;
@@ -89,6 +90,45 @@ function About(){
                 </div>
             </div>
         <Team />
+         <footer>
+               <div className='section pt-5 pb-5'>
+               {langActive ? ukrItem.map(item => ( <div className='contacts-container'>
+                 <h3 className='heading mb-5'>{item['ukr-footer-heading']}</h3>
+                   <div className='info'>
+                   <h5>{item['ukr-footer-name-h']}</h5>
+                   <p>
+                         {item['ukr-footer-name-desc']}
+                   </p>
+                   <h5>{item['ukr-footer-address-h']}</h5>
+                   <span>{item['ukr-footer-address-desc']}</span>
+                 </div>
+                 <div className='info'>
+                   <h5>{item['ukr-footer-social']}</h5>
+                   <span><a href="https://www.facebook.com/Країна-Миру-Country-of-Peace-112502804794164/"><BsFacebook/> Facebook</a></span>
+                   <span><a href="https://instagram.com/countryofpeace.ua?igshid=YmMyMTA2M2Y/"><BsInstagram/> Instagram</a></span>  
+                   <h5 className='mt-4'>{item['ukr-footer-phone']}</h5>
+                   <span>+380 60 983 08 42</span><br/>
+                 </div>
+               </div>)) : enItem.map(item => ( <div className='contacts-container'>
+                 <h3 className='heading mb-5'>{item['en-footer-heading']}</h3>
+                   <div className='info'>
+                   <h5>{item['en-footer-name-h']}</h5>
+                   <p>
+                         {item['en-footer-name-desc']}
+                   </p>
+                   <h5>{item['en-footer-address-h']}</h5>
+                   <span>{item['en-footer-address-desc']}</span>
+                 </div>
+                 <div className='info'>
+                   <h5>{item['en-footer-social']}</h5>
+                   <span><a href="https://www.facebook.com/Країна-Миру-Country-of-Peace-112502804794164/"><BsFacebook/> Facebook</a></span>
+                   <span><a href="https://instagram.com/countryofpeace.ua?igshid=YmMyMTA2M2Y/"><BsInstagram/> Instagram</a></span>  
+                   <h5 className='mt-4'>{item['en-footer-phone']}</h5>
+                   <span>+380 60 983 08 42</span><br/>
+                 </div>
+               </div>))
+             </div>
+           </footer>                   
         </div>
     )
 
