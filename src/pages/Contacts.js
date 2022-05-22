d5d9415ab6300b45eb5c8bf24d6f8ca55f4748d7
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 11:13:13 +0300

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -43,20 +43,33 @@ function Contacts(){
                  </Navbar.Collapse>
                </Navbar>
            </header>
-        <div className="contacts-page">
+         {langActive ? ukrItem.map(item => ( <div className="contacts-page">
             <div className="contacts-item">
                 <FaPhone/>
                 <span>+380 XX XXX XX XX</span>
             </div>
             <div className="contacts-item">
                 <MdLocationOn/>
-                <span>121 Rexford Str., West-Side Av., New York NY-92103-9000</span>
+                <span>{item['ukr-footer-address-desc']}</span>
             </div>
             <div className="contacts-item">
                 <HiMail/>
                 <span>countryofpeace@gmail.com</span>
             </div>
-        </div>
+        </div> )) : enItem.map(item => ( <div className="contacts-page">
+            <div className="contacts-item">
+                <FaPhone/>
+                <span>+380 XX XXX XX XX</span>
+            </div>
+            <div className="contacts-item">
+                <MdLocationOn/>
+                <span>{item['en-footer-address-desc']}</span>
+            </div>
+            <div className="contacts-item">
+                <HiMail/>
+                <span>countryofpeace@gmail.com</span>
+            </div>
+        </div> ))
                 <footer>
                <div className='section pt-5 pb-5'>
                {langActive ? ukrItem.map(item => ( <div className='contacts-container'>
