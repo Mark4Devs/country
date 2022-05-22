bd60589ea02f62e11e868aabb4f3158a359b87c2
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 03:09:54 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,30 @@ function Home() {
         setLang(false);
       };
     return (
+         <div>
+              <header className="header">
+               <Navbar className='bg-light' expand="lg" fixed='top'>
+               <div className='header-outline'>
+                   <Button>English</Button>
+                   <Button className='btn active'>Українська</Button>
+
+                 <div className='header-info'>
+                   <span>+380 60 983 08 42</span>
+                   <span>countryofpeace@gmail.com</span>
+                 </div>
+               </div>
+                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                 <Navbar.Brand to="/"><img src='images/logo.png'/></Navbar.Brand>
+                 <Navbar.Collapse className='nav-bar'>
+                   <Nav className='navigation'>
+                     <NavLink to="/" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Головна</NavLink>
+                     <NavLink to="/about" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Про нас</NavLink>
+                     <NavLink to="/donate" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Реквізити</NavLink>
+                     <NavLink to="/contacts" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Контакти</NavLink>
+                   </Nav>
+                 </Navbar.Collapse>
+               </Navbar>
+           </header>
         <div className='home'>
                       {langActive ? ukrItem.map(item => ( <Carousel>
                 <Carousel.Item>
@@ -97,6 +121,7 @@ function Home() {
             </div>
           </div>
         </div> 
+     </div>
     )
 }
 
