b89d688d71d27aa312c6058ca5b00c419548f282
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2022-05-22 09:06:37 +0300

    Update Donate.js

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -4,29 +4,29 @@ import {  NavLink } from 'react-router-dom';
 function Donate(){
     return(
         <div>
-        <header className="header">
-          <Navbar className='bg-light' expand="lg" fixed='top'>
-          <div className='header-outline'>
-              <Button>English</Button>
-              <Button className='btn active'>Українська</Button>
+            <header className="header">
+               <Navbar className='bg-light' expand="lg" fixed='top'>
+               <div className='header-outline'>
+                   <Button onClick={toggleLangNext}>English</Button>
+                   <Button onClick={toggleLang} className='btn active'>Українська</Button>
 
-            <div className='header-info'>
-              <span>+380 60 983 08 42</span>
-              <span>countryofpeace@gmail.com</span>
-            </div>
-          </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Brand to="/"><img src='images/logo.png'/></Navbar.Brand>
-            <Navbar.Collapse className='nav-bar'>
-              <Nav className='navigation'>
-                <NavLink to="/" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Головна</NavLink>
-                <NavLink to="/about" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Про нас</NavLink>
-                <NavLink to="/donate" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Реквізити</NavLink>
-                <NavLink to="/contacts" className={({ isActive }) => (isActive ? 'link active' : 'link')}>Контакти</NavLink>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-      </header>
+                 <div className='header-info'>
+                   <span>+380 60 983 08 42</span>
+                   <span>countryofpeace@gmail.com</span>
+                 </div>
+               </div> 
+                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                 <Navbar.Brand to="/"><img src='images/logo.png'/></Navbar.Brand>
+                 <Navbar.Collapse className='nav-bar'>
+                   <Nav className='navigation'>
+                     {langActive ? ukrItem.map(item => (<NavLink to="/" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['ukr-menu-1']}</NavLink>)) : enItem.map(item => (<NavLink to="/" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['en-menu-1']}</NavLink>))} 
+                     {langActive ? ukrItem.map(item => (<NavLink to="/about" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['ukr-menu-2']}</NavLink>)) : enItem.map(item => (<NavLink to="/abot" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['en-menu-2']}</NavLink>))} 
+                     {langActive ? ukrItem.map(item => (<NavLink to="/donate" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['ukr-menu-3']}</NavLink>)) : enItem.map(item => (<NavLink to="/donate" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['en-menu-3']}</NavLink>))} 
+                     {langActive ? ukrItem.map(item => (<NavLink to="/contacts" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['ukr-menu-4']}</NavLink>)) : enItem.map(item => (<NavLink to="/contacts" className={({ isActive }) => (isActive ? 'link active' : 'link')}>{item['en-menu-4']}</NavLink>))} 
+                   </Nav>
+                 </Navbar.Collapse>
+               </Navbar>
+           </header>
         <div className="container-donate">
             <div className="section">
                 <ul>
